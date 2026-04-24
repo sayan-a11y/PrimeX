f6d6c7de467e00662fa33e2f2ec3625b7d7a588d
sayan-a11y <sayan@example.com>
2026-04-24 20:13:53 +0530

    Update backend to use MONGODB_URI

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
